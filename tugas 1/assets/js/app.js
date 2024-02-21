e98fafb2fdffca01cbf4775d75d418d84e2d1af3
bowonieh <aguswibowo.amd@gmail.com>
2024-02-21 22:16:25 +0700

    tugas 1 css magician

---
+++ tugas 1/assets/js/app.js
@@ -0,0 +1,5 @@
+import * as bootstrap from "../../node_modules/bootstrap/dist/js/bootstrap.js";
+window.bootstrap = bootstrap
+
+import '../../node_modules/jquery/dist/jquery.js';
+window.$ = window.jQuery = jQuery;
